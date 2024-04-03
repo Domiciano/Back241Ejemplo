e795681f7ecfbbfd93673f670e34a0dd152281d0
Domiciano Rincón <domiciano.rincon@correounivalle.edu.co>
2024-04-03 13:01:08 -0500

    Template added

---
+++ src/main/java/com/example/backendespiral/BackendEspiralApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendespiral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEspiralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEspiralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendespiral/BackendEspiralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendespiral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEspiralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
